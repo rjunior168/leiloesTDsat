b498a20d05cd8fb72fe03873a75d212e29cbbec8
junior <rodrigoccjunior@gmail.com>
2024-12-10 13:28:36 -0300

    implementar funcionalidade de salvar itens no banco e uma mensagem informando se o cadastri foi um sucesso.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,7 +1,6 @@
-public class MainApp {
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new cadastroVIEW().setVisible(true);
-        });
-    }
+public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+        new CadastroVIEW().setVisible(true);
+    });
 }
+
