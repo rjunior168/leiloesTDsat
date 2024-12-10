fe6e1d87b044767d9fe3f3ba86eeb1ec7e858211
junior <rodrigoccjunior@gmail.com>
2024-12-10 11:53:53 -0300

    Definir cadastroVIEW como tela principal

---
+++ src/MainApp.java
@@ -0,0 +1,7 @@
+public class MainApp {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new cadastroVIEW().setVisible(true);
+        });
+    }
+}
